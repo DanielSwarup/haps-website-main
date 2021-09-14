fc3ae577562c12de9f085592b124a77d904f4be4
Anichoo <68532349+Anichoo@users.noreply.github.com>
2021-09-14 10:04:06 +0100

    Update Publication.vue

--- components/Publication/Publication.vue
+++ components/Publication/Publication.vue
@@ -11,7 +11,7 @@
             Reinforcement learning versus swarm intelligence for autonomous multi-HAPS coordination
           </div>
           <div class="publication-content">
-            This work analyses the performance of Reinforcement Learning (RL) versus Swarm Intelligence (SI) for coordinating multiple unmanned High Altitude Platform Stations (HAPS) for communications area coverage. It builds upon previous work which looked at various elements of both algorithms. The main aim of this paper is to address the continuous state-space challenge within this work by using partitioning to manage the high dimensionality problem. This enabled comparing the performance of the classical cases of both RL and SI establishing a baseline for future comparisons of improved versions. From previous work, SI was observed to perform better across various key performance indicators. However, after tuning parameters and empirically choosing suitable partitioning ratio for the RL state space, it was observed that the SI algorithm still maintained superior coordination capability by achieving higher mean overall user coverage (about 20% better than the RL algorithm), in addition to faster convergence rates. Though the RL technique showed better average peak user coverage, the unpredictable coverage dip was a key weakness, making SI a more suitable algorithm within the context of this work.
+            This work analyses the performance of Reinforcement Learning (RL) versus Swarm Intelligence (SI) for coordinating multiple unmanned High Altitude Platform Stations (HAPS) for communications area coverage. It builds upon previous work which looked at various elements of both algorithms. The main aim of this paper is to address the continuous state-space challenge within this work by using partitioning to manage the high dimensionality problem. 
             <br>
             <br>
            Anicho, O., Charlesworth, P.B., Baicher, G.S. et al. Reinforcement learning versus swarm intelligence for autonomous multi-HAPS coordination. SN Appl. Sci. 3, 663 (2021).
@@ -56,7 +56,7 @@
             Implementing Solar-Powered HAPS for Rural Broadband Connectivity: Concepts, Challenges & Mitigation
           </div>
           <div class="publication-content">
-           This paper examines the implementation of solar-powered High Altitude Platform Stations (HAPS) for rural broadband connectivity. It outlines some technical considerations and concepts associated with implementing HAPS as a communications infrastructure. To realise the potentials of solar HAPS for rural broadband connectivity, some key technology, business and policy questions must be addressed. For instance, to meet service demands, the solar-HAPS platform must remain aloft for long periods without running out of power (endurance), which is a technology challenge. Also relevant are non-technology issues like fitness-for-purpose and business viability, which are often overshadowed by technology problems but yet consequential. An aggregation and analysis of these implementation concepts may be helpful for both technology and policy decisions in the bid to address rural connectivity gaps.
+           This paper examines the implementation of solar-powered High Altitude Platform Stations (HAPS) for rural broadband connectivity. It outlines some technical considerations and concepts associated with implementing HAPS as a communications infrastructure. To realise the potentials of solar HAPS for rural broadband connectivity, some key technology, business and policy questions must be addressed.
             <br>
             <br>
            O. Anicho, P. B. Charlesworth, G. S. Baicher and A. Nagar, "Implementing Solar-Powered HAPS for Rural broadband Connectivity: Concepts, Challenges & Mitigation," 2020 IEEE Global Humanitarian Technology Conference (GHTC), 2020, pp. 1-8, doi: 10.1109/GHTC46280.2020.9342894.
@@ -101,7 +101,7 @@
             Situation Awareness and Routing Challenges in Unmanned HAPS/UAV based Communications Networks
           </div>
           <div class="publication-content">
-            This paper examines Situation Awareness (SA) as a factor in addressing routing challenges in HAPS/UAV based networks especially in multi-HAPS/UAV implementations. Routing in UAV-based networks is a critical element for successful transmission of data from source to destination node. However, in UAV based networks the concept of routing assumes a more challenging dimension mainly due to the mobility of the vehicles or platforms. This paper highlights how situation awareness can impact routing decisions and consequently improve network throughput. It is suggested that SA should be considered a factor in mitigating routing challenges in UAV based networks.
+            This paper examines Situation Awareness (SA) as a factor in addressing routing challenges in HAPS/UAV based networks especially in multi-HAPS/UAV implementations. 
             <br>
             <br>
             O. Anicho, P. B. Charlesworth, G. S. Baicher and A. Nagar, "Situation Awareness and Routing Challenges in Unmanned HAPS/UAV based Communications Networks," 2020 International Conference on Unmanned Aircraft Systems (ICUAS), 2020, pp. 1175-1182, doi: 10.1109/ICUAS48674.2020.9214015.
@@ -146,7 +146,7 @@
             Reinforcement Learning for Multiple HAPS/UAV Coordination: Impact of Exploration–Exploitation Dilemma on Convergence
           </div>
           <div class="publication-content">
-            This work analyses the application of reinforcement learning (RL) for coordinating multiple unmanned high-altitude platform stations (HAPS) or unmanned aerial vehicles (UAVs) for providing communications area coverage to a community of fixed and mobile users. Multiple agent coordination techniques are essential for developing autonomous capabilities for multi-UAV/HAPS control and management. This paper examines the impact of exploration–exploitation dilemma on the application of RL for coordinating multiple UAVs/HAPS. In the work, it is observed that RL convergence is a challenge, as the RL algorithm struggles to find optimal positioning for maximum user coverage. This paper attempts to establish the source of the convergence issue with the RL technique for this specific application scenario. The work goes on to suggest methods to minimise this impact, and some insights for applying RL techniques for multi-agent coordination for communications area coverage.
+            This paper examines the impact of exploration–exploitation dilemma on the application of RL for coordinating multiple UAVs/HAPS.
             <br>
             <br>
             Anicho O., Charlesworth P.B., Baicher G.S., Nagar A.K. (2020) Reinforcement Learning for Multiple HAPS/UAV Coordination: Impact of Exploration–Exploitation Dilemma on Convergence. In: Nagar A., Deep K., Bansal J., Das K. (eds) Soft Computing for Problem Solving 2019. Advances in Intelligent Systems and Computing, vol 1138. Springer, Singapore. https://doi.org/10.1007/978-981-15-3290-0_12
